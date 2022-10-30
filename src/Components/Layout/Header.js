08f3eed032c5ea2c07d76a800c4c89d45e58939d
muzammilshaikh12 <110051885+muzammilshaikh12@users.noreply.github.com>
2022-10-30 23:12:45 +0530

    Header Created

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from "react";
+
+import CartButton  from "./CartBtn";
+
+import "./Header.css";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className="header">
+        <h1>Muzz_Meals</h1>
+       <CartButton />
+      </header>
+      <div className="main-image">
+        <img
+          src='https://github.com/academind/react-complete-guide-code/blob/11-practice-food-order-app/extra-files/meals.jpg?raw=true'
+          alt='Yummy Food!!!'
+        />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
